441f2f5997293d8d9dbaf3d5ac9fd4a6bf1e330b
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-16 13:51:59 +0200

    reafactor routing

--- booking-rooms/src/App.jsx
+++ booking-rooms/src/App.jsx
@@ -1,11 +1,47 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./page/home/Home";
-
+import Login from "./page/login/Login";
+import Booking from "./page/booking/Booking";
+import User from "./page/user/User";
+import SidebarWrapper from "./components/wrapper/SideBarWrapper";
 function App() {
   return (
-    <div className="container w-full">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <SidebarWrapper>
+              <Home />
+            </SidebarWrapper>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/booking"
+          element={
+            <SidebarWrapper>
+              <Booking />
+            </SidebarWrapper>
+          }
+        />
+        <Route
+          path="/user"
+          element={
+            <SidebarWrapper>
+              <User />
+            </SidebarWrapper>
+          }
+        />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
--- booking-rooms/src/main.jsx
+++ booking-rooms/src/main.jsx
@@ -2,49 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Booking from "./page/booking/Booking.jsx";
-import SidebarWrapper from "./components/wrapper/SideBarWrapper.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import User from "./page/user/User.jsx";
-import Login from "./page/login/Login.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <SidebarWrapper>
-        <App />
-      </SidebarWrapper>
-    ),
-  },
-
-  {
-    path: "/booking",
-    element: (
-      <SidebarWrapper>
-        <Booking />
-      </SidebarWrapper>
-    ),
-  },
-  {
-    path: "/user",
-    element: (
-      <SidebarWrapper>
-        <User />
-      </SidebarWrapper>
-    ),
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
